10965b18789836aa03ac586efa7782488e38edd1
Ryan <themolenator@gmail.com>
2018-02-25 14:48:47 -0500

    Created new file
    added code to the file.

---
+++ script.py
@@ -0,0 +1,21 @@
+
+
+
+class calculator:
+    def addition(x,y):
+        added = x + y
+        print(added)
+
+    def subtration(x,y):
+        subtract = x - y
+        print(subtract)
+
+    def multiplication(x,y):
+        mult = x * y
+        print(mult)
+
+    def division(x,y):
+        div = x / y
+        print(div)
+
+calculator.multiplication(3,5)
